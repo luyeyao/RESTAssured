9014593386bf8b3fe79601c8578718af88360b4b
luyeyao <luyeyao@qq.com>
2020-05-20 20:42:39 +0800

    增加部门

--- src/test/java/secret/api/Depart.java
+++ src/test/java/secret/api/Depart.java
@@ -1,7 +1,10 @@
 package secret.api;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import secret.WeWork;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -13,12 +16,43 @@ public class Depart {
     public Response list(int id){
         return given()
                 .param("access_token", WeWork.getInstance().getToken())
-                .param("id",parentid)
-                .when()
+                //.param("id",parentid)
+            .when()
                 .get("https://qyapi.weixin.qq.com/cgi-bin/department/list")
-                .then()
+            .then()
                 .log().all()
                 .body("errcode",equalTo(0))
                 .extract().response();
     }//如果此处没有返回值，则调用list方法时报错
+
+    public Response addDep(String name,int parentid){
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("name", name);
+        data.put("parentid",parentid);
+
+        return given()
+                .log().all()
+                .queryParam("access_token",WeWork.getInstance().getToken())
+                .contentType(ContentType.JSON)
+                .body(data)
+                .when().log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/create")
+                .then()
+                .log().all()
+                .body("errcode",equalTo(0))
+                .extract().response();
+    }
+    public Response addDep(String name){
+        return addDep(name,parentid);
+    }
+
+    public Response delDep(int id){
+        return given()
+                .param("access_token",WeWork.getInstance().getToken())
+                .when().get("https://qyapi.weixin.qq.com/cgi-bin/department/delete")
+                .then()
+                .body("errmsg",equalTo("deleted"))
+                .extract().response();
+
+    }
 }
--- src/test/java/secret/testcase/DepartTest.java
+++ src/test/java/secret/testcase/DepartTest.java
@@ -1,7 +1,6 @@
 package secret.testcase;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import secret.api.Depart;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,13 +11,21 @@ public class DepartTest {
 
     static Depart depart = new Depart();
     @Test
-    public void addDepart(){}
+    public void addDepart(){
+        depart.addDep("研发部",depart.parentid)
+                .then()
+                .body("errmsg",equalTo("created"));
+
+        depart.list(depart.parentid).body("",equalTo(""));
+    }
     @Test
     public void delDepart(){}
     @Test
     public void list(){
         depart.list(depart.parentid)
                 .then()
+                .log().all()
                 .body("errmsg",equalTo("ok"));
     }
+
 }
